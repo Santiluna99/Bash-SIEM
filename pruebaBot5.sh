f4efa7e92023a83c8b6cb449ebaf8b061fe739c4
Santiluna99 <92774109+Santiluna99@users.noreply.github.com>
2024-03-14 18:46:23 -0300

    Update pruebaBot5.sh

--- pruebaBot5.sh
+++ pruebaBot5.sh
@@ -13,10 +13,10 @@ PATTERN="incorrect password\|authentication failure"
 FAILED_LOGINS=$(strings "$LOG_FILE" | grep "$PATTERN" | tail -n 5)
 
 # Token del bot de Telegram
-TOKEN="7153902856:AAFuyErx3lMEswAmLBBIBbWXIZjHN2-MTEU"
+TOKEN="tu_token"
 
 # ID del chat de Telegram al que se enviará el mensaje
-CHAT_ID="-4173191777"
+CHAT_ID="tu_id"
 
 # Verificar si se encontraron intentos de inicio de sesión fallidos
 if [ -n "$FAILED_LOGINS" ]; then
@@ -24,7 +24,7 @@ if [ -n "$FAILED_LOGINS" ]; then
     TIME_STAMPS=$(echo "$FAILED_LOGINS" | grep "$PATTERN" | awk '{print $3}')
 
     # Crear el mensaje de alerta con las horas de los intentos de inicio de sesión fallidos
-    ALERT_MESSAGE="Lord Senior Santiago, se detectaron los siguientes intentos de inicio de sesión fallidos en el archivo de registro de autenticación ($LOG_FILE) a las siguientes horas: $TIME_STAMPS"
+    ALERT_MESSAGE="Se detectaron los siguientes intentos de inicio de sesión fallidos en el archivo de registro de autenticación ($LOG_FILE) a las siguientes horas: $TIME_STAMPS"
 
     # Enviar la alerta a Telegram
     curl -s -X POST https://api.telegram.org/bot$TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="$ALERT_MESSAGE" > /dev/null
